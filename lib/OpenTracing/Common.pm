379d459783978c5e30b878fc176d26ce40c4b72e
Tom Molesworth <TEAM@cpan.org>
2019-06-16 21:55:59 +0800

    Update documentation for next release

--- lib/OpenTracing/Common.pm
+++ lib/OpenTracing/Common.pm
@@ -5,10 +5,32 @@ use warnings;
 
 # VERSION
 
+=encoding utf8
+
+=head1 NAME
+
+OpenTracing::Common - provides common logic between OpenTracing classes
+
+=head1 DESCRIPTION
+
+No user-serviceable parts inside.
+
+=cut
+
 sub new {
     my ($class, %args) = @_;
     bless \%args, $class
 }
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Tom Molesworth <TEAM@cpan.org>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2018-2019. Licensed under the same terms as Perl itself.
 
--- lib/OpenTracing/Process.pm
+++ lib/OpenTracing/Process.pm
@@ -7,12 +7,56 @@ use warnings;
 
 use parent qw(OpenTracing::Common);
 
+=encoding utf8
+
+=head1 NAME
+
+OpenTracing::Process - information about a single process
+
+=head1 DESCRIPTION
+
+Each batch of spans is linked to a process. This can either be a Unix-style process or a more abstract "service"
+concept.
+
+=cut
+
+=head1 METHODS
+
+=head2 name
+
+The process name. Freeform text string.
+
+=cut
+
 sub name { shift->{name} }
+
+=head2 tags
+
+Arrayref of tags relating to the process.
+
+=cut
+
 sub tags { shift->{tags} }
+
+=head2 tag_list
+
+List of tags for this process.
+
+=cut
+
 sub tag_list {
-    # OpenTracing::Tag->new(key => 'example_process_tag', value => 'process_value')
     (shift->{tags} //= [])->@*
 }
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Tom Molesworth <TEAM@cpan.org>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2018-2019. Licensed under the same terms as Perl itself.
 
